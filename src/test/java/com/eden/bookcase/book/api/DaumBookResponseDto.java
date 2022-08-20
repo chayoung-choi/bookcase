5c321e7fb9429a5f82a7e706d0a385b526019fbc
chayoung-choi <chayoung.dev@gmail.com>
2022-08-20 17:21:26 +0900

    test: Daum_책_조회

--- src/test/java/com/eden/bookcase/book/api/DaumBookResponseDto.java
+++ src/test/java/com/eden/bookcase/book/api/DaumBookResponseDto.java
@@ -5,11 +5,30 @@ import lombok.Data;
 
 import java.util.List;
 
-@JsonIgnoreProperties(ignoreUnknown =true) // 선언 필드 외에 모든 요소 제외
+@JsonIgnoreProperties(ignoreUnknown=true) // 선언 필드 외에 모든 요소 제외
 @Data
 public class DaumBookResponseDto {
   private List<DaumBookDto> documents;
-  private boolean is_end;
-  private int pageable_count;
-  private int total_count;
+  private Meta meta;
+
+  @JsonIgnoreProperties(ignoreUnknown=true)
+  @Data
+  static class DaumBookDto {
+    private String title;
+    private String url;
+    private String thumbnail;
+    private String[] authors;
+    private String contents;
+    private String datetime;
+    private String isbn;
+    private String publisher;
+  }
+
+  @JsonIgnoreProperties(ignoreUnknown=true)
+  @Data
+  static class Meta {
+    private boolean is_end;
+    private int pageable_count;
+    private int total_count;
+  }
 }
