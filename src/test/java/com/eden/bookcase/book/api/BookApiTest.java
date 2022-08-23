cf5389e0856adbfae84d7945bc7e8d5c31ca6e73
chayoung-choi <chayoung.dev@gmail.com>
2022-08-23 18:08:14 +0900

    test: Daum 책 조회 API

--- src/test/java/com/eden/bookcase/book/api/BookApiTest.java
+++ src/test/java/com/eden/bookcase/book/api/BookApiTest.java
@@ -1,38 +1,40 @@
 package com.eden.bookcase.book.api;
 
-import com.eden.bookcase.book.dto.DaumBookResponseDto;
+import com.eden.bookcase.book.service.BookService;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
-import org.springframework.http.HttpMethod;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
-import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Slf4j
-@RestClientTest(BookApi.class)
+@WebMvcTest(BookApi.class)
 public class BookApiTest {
-  @Autowired
-  private MockRestServiceServer mockServer;
 
+  @MockBean
+  BookService bookService;
+
+  @Autowired
+  private MockMvc mvc;
+
+  @DisplayName("[API][GET] Daum 책 조회")
   @Test
-  void Daum_책_조회_API() {
+  void getDaumBook() throws Exception {
     MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
     params.add("query", "개발자");
 
-    DaumBookResponseDto daumBookResponseDto = new DaumBookResponseDto();
-
-    mockServer
-        .expect(requestTo("/daum/book"))
-        .andExpect(method(HttpMethod.GET))
-        .andRespond(withSuccess(daumBookResponseDto.toString(), MediaType.APPLICATION_JSON));
-
-    log.info(daumBookResponseDto.toString());
+    mvc.perform(get("/daum/book", params))
+            .andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andDo(print());
   }
 }
